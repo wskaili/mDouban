17f147ba94c7578e3e5edc84efade785af9c4262
wskaili <stepgreatto@163.com>
2018-10-07 22:01:44 +0800

    init

---
+++ miniprogram/pages/index/index.js
@@ -0,0 +1,59 @@
+//index.js
+const app = getApp()
+
+Page({
+  data: {
+    page: 1,
+    count: 20,
+    movies: [],
+    type: 'in_theaters',
+    hasMore: true,
+  },
+
+  onLoad: function() {
+    this.loadData()
+  },
+  onReady: function() {
+    wx.setNavigationBarTitle({
+      title: '正在热映'
+    })
+  },
+  loadData: function() {
+    wx.showLoading({
+      title: '正在加载中',
+    })
+    return app.douban.find(this.data.type, this.data.page++, this.data.count)
+      .then(data => {
+        if (data.subjects && data.subjects.length) {
+          this.setData({
+            movies: this.data.movies.concat(data.subjects)
+          })
+        } else {
+          this.setData({
+            hasMore: false
+          })
+        }
+       
+        // stop loading
+        wx.hideLoading()
+      })
+  },
+  // 下拉刷新
+  onPullDownRefresh: function() {
+    this.setData({
+      page: 1,
+      hasMore: false,
+      movies: []
+    })
+    this.loadData()
+      .then(() => {
+        wx.stopPullDownRefresh()
+      })
+  },
+  // 上拉刷新
+  onReachBottom: function() {
+    if (this.data.hasMore) {
+      this.loadData()
+    }
+  }
+})
